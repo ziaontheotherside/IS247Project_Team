e462e9cf4bf22596e9ccf8e3a60244800c46ee16
grace-ahn2 <graceahn41@gmail.com>
2025-04-13 10:47:29 -0400

    user class

---
+++ src/UserInfo.java
@@ -0,0 +1,52 @@
+public class UserInfo {
+    private double height;
+    private double weight;
+    private double goal_weight;
+    private int age;
+    private char gender;
+
+
+    public UserInfo() {};
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public double getGoal_weight() {
+        return goal_weight;
+    }
+
+    public void setGoal_weight(double goal_weight) {
+        this.goal_weight = goal_weight;
+    }
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+}
+
+}
